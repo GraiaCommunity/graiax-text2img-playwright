20015937db74c3f47b27288a86bca6929566478e
Redlnn <w731347477@gmail.com>
2022-09-13 22:33:04 +0800

    允许内置文转图方法直接返回html源码

--- src/graiax/text2img/playwright/builtin.py
+++ src/graiax/text2img/playwright/builtin.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, Literal, Optional, Union, overload
 
 from jinja2 import Template
 from markdown_it import MarkdownIt
@@ -30,32 +30,80 @@ markdown_it = (
 )
 
 
+@overload
 async def template2img(
     template: str,
     render_args: Dict[str, str],
+    html: Literal[False] = False,
     *,
     context_args: Optional[NewPageParms] = None,
     screenshot_args: Optional[ScreenshotParms] = None,
 ) -> bytes:
+    ...
+
+
+@overload
+async def template2img(
+    template: str,
+    render_args: Dict[str, str],
+    html: Literal[True] = True,
+) -> str:
+    ...
+
+
+async def template2img(
+    template: str,
+    render_args: Dict[str, str],
+    html: bool = False,
+    *,
+    context_args: Optional[NewPageParms] = None,
+    screenshot_args: Optional[ScreenshotParms] = None,
+) -> Union[str, bytes]:
     """Jinja2 模板转图片
 
     Args:
         template (str): Jinja2 模板
         render_args (Dict[str, str]): Jinja2.Template.render 的参数
+        html (bool): 返回生成的 HTML 代码而不是图片生成结果的 bytes
         context_args (ContextParm, optional): Playwright 浏览器 new_context 方法的参数
         screenshot_args (ScreenshotParms, optional): Playwright 浏览器页面截图方法的参数
     """
-    return await html2img(Template(template).render(**render_args), context_args, screenshot_args)
+    html_code: str = Template(template).render(**render_args)
+    return html_code if html else await html2img(html_code, context_args, screenshot_args)
+
+
+@overload
+async def text2img(
+    text: str,
+    disable_default_css: bool = False,
+    extra_css: str = "",
+    html: Literal[False] = False,
+    *,
+    context_args: Optional[NewPageParms] = None,
+    screenshot_args: Optional[ScreenshotParms] = None,
+) -> bytes:
+    ...
+
+
+@overload
+async def text2img(
+    text: str,
+    disable_default_css: bool = False,
+    extra_css: str = "",
+    html: Literal[True] = True,
+) -> str:
+    ...
 
 
 async def text2img(
     text: str,
     disable_default_css: bool = False,
     extra_css: str = "",
+    html: bool = False,
     *,
     context_args: Optional[NewPageParms] = None,
     screenshot_args: Optional[ScreenshotParms] = None,
-) -> bytes:
+) -> Union[str, bytes]:
     """纯文本转图片
 
     若使用 HTML 代码请在一行写完，换行会直接分段
@@ -64,32 +112,61 @@ async def text2img(
         text (str): 要转换为图片的文本
         disable_default_css (bool): 是否禁止使用内置 CSS
         extra_css (str): 除了内置 CSS 外需要在生成的页面中使用的 CSS
+        html (bool): 返回生成的 HTML 代码而不是图片生成结果的 bytes
         context_args (ContextParm, optional): Playwright 浏览器 new_context 方法的参数
         screenshot_args (ScreenshotParms, optional): Playwright 浏览器页面截图方法的参数
     """
-    text = (
+    html_code = (
         '<meta name="viewport" content="width=device-width,initial-scale=1.0">'
         f'<style>{extra_css}{index_css if disable_default_css else ""}</style>'
         f'<div class="container">{text2html(text)}</div>'
     )
-    return await html2img(text, context_args, screenshot_args)
+    return html_code if html else await html2img(html_code, context_args, screenshot_args)
+
+
+@overload
+async def md2img(
+    content: str,
+    disable_default_css: bool = False,
+    extra_css: str = "",
+    html: Literal[False] = False,
+    *,
+    disable_onedark_css: bool = False,
+    context_args: Optional[NewPageParms] = None,
+    screenshot_args: Optional[ScreenshotParms] = None,
+) -> bytes:
+    ...
+
+
+@overload
+async def md2img(
+    content: str,
+    disable_default_css: bool = False,
+    extra_css: str = "",
+    html: Literal[True] = True,
+    *,
+    disable_onedark_css: bool = False,
+) -> str:
+    ...
 
 
 async def md2img(
     content: str,
     disable_default_css: bool = False,
     extra_css: str = "",
+    html: bool = False,
     *,
     disable_onedark_css: bool = False,
     context_args: Optional[NewPageParms] = None,
     screenshot_args: Optional[ScreenshotParms] = None,
-) -> bytes:
+):
     """Markdown 文本转图片
 
     Args:
         content (str): 要转换为图片的 Markdown 文本
         disable_default_css (bool): 是否禁止使用内置 CSS
         extra_css (str): 除了内置 CSS 外需要在生成的页面中使用的 CSS
+        html (bool): 返回生成的 HTML 代码而不是图片生成结果的 bytes
         disable_onedark_css (bool): 是否禁用内置的用于代码块高亮的 OneDark 主题，
             可通过 extra_css 参数传入其他适用于 pygments 生成结果的 CSS
         context_args (ContextParm, optional): Playwright 浏览器 new_context 方法的参数
@@ -102,10 +179,10 @@ async def md2img(
         github_css = Path(Path(__file__).parent / "css" / "github.css").read_text()
         onedark_css = "" if disable_onedark_css else Path(Path(__file__).parent / "css" / "one-dark.css").read_text()
 
-    md = (
+    html_code = (
         '<meta name="viewport" content="width=device-width,initial-scale=1.0">'
         f'<style>{onedark_css}{github_css}{extra_css}{"" if disable_default_css else index_css}</style>'
         f'<div class="markdown-body">{markdown_it.render(content)}</div>'
     )
 
-    return await html2img(md, context_args, screenshot_args)
+    return html_code if html else await html2img(html_code, context_args, screenshot_args)
