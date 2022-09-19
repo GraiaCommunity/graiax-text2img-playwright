d4954f5206370b2737941c98b95dc20535808465
Redlnn <w731347477@gmail.com>
2022-09-19 20:59:05 +0800

    移动注释位置

--- src/graiax/text2img/playwright/api.py
+++ src/graiax/text2img/playwright/api.py
@@ -1,20 +1,3 @@
-"""
-https://mdit-py-plugins.readthedocs.io/en/latest/
-mdit-py-plugins 的 自带可用插件列表：
-  - mdit_py_plugins.amsmath: （数学公式？）markdown-it-amsmath
-  - mdit_py_plugins.anchors: 标题锚点 markdown-it-anchors
-  - mdit_py_plugins.container: markdown-it-container
-  - mdit_py_plugins.deflist: markdown-it-deflist
-  - mdit_py_plugins.dollarmath: （数学公式？）markdown-it-dollarmath
-  - mdit_py_plugins.footnote: 脚注 markdown-it-footnote
-  - mdit_py_plugins.front_matter: markdown-it-front-matter
-  - mdit_py_plugins.myst_blocks: ???
-  - mdit_py_plugins.myst_role: ???
-  - mdit_py_plugins.tasklists: 任务清单 markdown-it-task-lists
-  - mdit_py_plugins.texmath: （数学公式？）markdown-it-texmath
-  - mdit_py_plugins.wordcount: ???
-"""
-
 from typing import Optional
 
 from graiax.playwright import PlaywrightBrowser
